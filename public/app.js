ef59a616fabc0a6279b6c2d6090740ab6243f794
ed <ejbyne@gmail.com>
2014-12-07 11:44:05 +0000

    added spinner, error message and refactoring

--- public/app.js
+++ public/app.js
@@ -1,22 +1,31 @@
 function addProfileFromUsername(username){
+  $('.spinner').show();
   $.get('https://api.github.com/users/'+username+'?client_id=058399f7ffb7aabf5c62&client_secret=92289716e9f45636be4bdf18cd374493d1f42dce', function(user){
     var newProfile = Mustache.render($('#profile-template').html(), user);
     $(newProfile).prependTo('.profile-container').hide().slideDown();
   }).error(function () {
-    alert ("sorry, username not found");
+    $('.modal').addClass('shown');
   }).always(function(){
     $('#username').val('');
+    $('.spinner').hide();
   })
 }
 
 $(document).ready(function() {
+
   $('#add_profile').submit(function(event) {
     event.preventDefault();
     addProfileFromUsername($('#username').val())
   });
+
   $('.profile-container').on('click', '.close', function() {
     $(this).closest('.profile').slideUp(function() {
       $(this).remove();
     })
   });
+  
+  $('.modal .close').on('click', function() {
+    $('.modal').removeClass('shown');
+  })
+
 });
